aecd96fed9e12506ada120ee4170425c4b26c026
Aleksei Ogoltcov <aaogoltcov@mail.ru>
2021-06-14 21:59:50 +0300

    mistakes resolved

--- src/js/Polling.js
+++ src/js/Polling.js
@@ -1,17 +1,19 @@
 'use strict';
 
 import { ajax } from 'rxjs/ajax';
-import {map, catchError } from 'rxjs/operators';
-import {of, throwError} from 'rxjs';
+import {map, catchError, switchMap} from 'rxjs/operators';
+import {interval, of, throwError} from 'rxjs';
 
 export default class Polling {
   constructor() {
+    this.url = 'https://jsrxjs.herokuapp.com/messages/unread';
     this.init();
   }
 
   init() {
-    ajax.getJSON(`https://jsrxjs.herokuapp.com/messages/unread`)
+    interval(1000)
       .pipe(
+        switchMap(() => ajax.getJSON(this.url)),
         map(response => response.messages),
         catchError(error => {
           if (error.status > 500) {
@@ -23,14 +25,17 @@ export default class Polling {
           return of(null);
         })
       )
-      .subscribe(messages => {
-        const alert = document.querySelector('.alert-primary');
-        if ( !(messages === null) ) {
-          if ( !alert.classList.contains('hidden') ) { alert.classList.add('hidden') }
-          messages.forEach(element => {
-            const subject = element.subject.length > 15 ? element.subject.substring(0, 14) + '...' : element.subject;
-            document.querySelector('tbody').insertAdjacentHTML('afterbegin',
-              `
+      .subscribe(messages => { this.drawMessages( messages ) })
+  }
+
+  drawMessages( messages ) {
+    const alert = document.querySelector('.alert-primary');
+    if ( !(messages === null) ) {
+      if ( !alert.classList.contains('hidden') ) { alert.classList.add('hidden') }
+      messages.forEach(element => {
+        const subject = element.subject.length > 15 ? element.subject.substring(0, 14) + '...' : element.subject;
+        document.querySelector('tbody').insertAdjacentHTML('afterbegin',
+          `
             <tr>
               <td><a href="##email-read.html">${ element.from }</a></td>
               <td><a href="##email-read.html"><i class="fa text-info m-r-15">
@@ -39,11 +44,9 @@ export default class Polling {
               <td class="text-right">${ element.received }</td>
             </tr>
           `)
-          })
-        } else {
-          if ( alert.classList.contains('hidden') ) { alert.classList.remove('hidden') }
-        }
       })
-    setInterval(this.init, 1000);
+    } else {
+      if ( alert.classList.contains('hidden') ) { alert.classList.remove('hidden') }
+    }
   }
 }
